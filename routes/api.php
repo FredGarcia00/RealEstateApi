ca5e3a1d6fbfa26a6534f6daa388b2a66d1120a7
Fred Garcia <Fred@Garcia.com>
2021-04-30 16:05:43 -0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+//posts 
+// crud is baisacally
+// 1. get all (GET) /api/posts
+// 2. create a post (post) /api/posts
+// 3. get a single (GET) /api/post/{id}
+// 4. update and single (put/patch) /api/posts/{id}
+// 5.delete (delete) /api/posts/{id}
+
+Route::get('/posts');
+
+//to create a resource (posts) in laravel
+//1. create the database and migrations
+//2. createa  model
+//2.5 create a service? Eloquent
+//3. create controller to get info from database
+//4. return that info
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
